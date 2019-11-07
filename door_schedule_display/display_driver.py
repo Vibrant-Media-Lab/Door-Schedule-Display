36266ad8734e597e9090603317fab69e702f9de4
Katie Morrison <codelikeagirl98@gmail.com>
2019-11-06 17:15:21 -0800

    adjusting spacing

--- door_schedule_display/display_driver.py
+++ door_schedule_display/display_driver.py
@@ -36,11 +36,10 @@ try:
     Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
     draw.text((10, 0), '20', font = font72, fill = 0)
-    draw.text((160, 0), 'Vibrant Media Lab', font = font48, fill = 0)
-    draw.line((255, 15, 360, 15), fill = 0)
-    draw.text((120, 20), 'Student Worker Schedule', font = font24, fill = 0)
-    draw.text((310, 0), '19', font = font72, fill = 0)  
-    draw.rectangle((20, 40, 560, 360), outline = 0)
+    draw.text((130, 0), 'Vibrant Media Lab', font = font48, fill = 0)
+    draw.text((120, 60), 'Student Worker Schedule', font = font24, fill = 0)
+    draw.text((610, 0), '19', font = font72, fill = 0)  
+    draw.rectangle((10, 80, 630, 360), outline = 0)
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
 
